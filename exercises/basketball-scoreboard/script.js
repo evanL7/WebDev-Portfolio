7f99d1f6381180763c3b6145c652ce928c9dee3e
evan <eevanlaw@gmail.com>
2024-05-01 17:54:52 -0700

    added JavaScript for reset button, custom font for the score, and CSS to look more like a scoreboard

--- exercises/basketball-scoreboard/script.js
+++ exercises/basketball-scoreboard/script.js
@@ -1,12 +1,16 @@
+// Home team variables
 let homeScore = document.getElementById("home");
 let homePlus1El = document.getElementById("home-plus-1-btn");
 let homePlus2El = document.getElementById("home-plus-2-btn");
 let homePlus3El = document.getElementById("home-plus-3-btn");
+let homeResetEl = document.getElementById("home-reset-btn");
 
+// Guest team variables
 let guestScore = document.getElementById("guest");
 let guestPlus1El = document.getElementById("guest-plus-1-btn");
 let guestPlus2El = document.getElementById("guest-plus-2-btn");
 let guestPlus3El = document.getElementById("guest-plus-3-btn");
+let guestResetEl = document.getElementById("guest-reset-btn");
 
 // Home team buttons
 homePlus1El.addEventListener("click", function () {
@@ -18,6 +22,9 @@ homePlus2El.addEventListener("click", function () {
 homePlus3El.addEventListener("click", function () {
     homeScore.textContent = parseInt(homeScore.textContent) + 3;
 });
+homeResetEl.addEventListener("click", function () {
+    homeScore.textContent = 0;
+});
 
 // Guest team buttons
 guestPlus1El.addEventListener("click", function () {
@@ -28,5 +35,8 @@ guestPlus2El.addEventListener("click", function () {
 });
 guestPlus3El.addEventListener("click", function () {
     guestScore.textContent = parseInt(guestScore.textContent) + 3;
+});
+guestResetEl.addEventListener("click", function () {
+    guestScore.textContent = 0;
 });
     
