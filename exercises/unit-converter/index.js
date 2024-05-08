0222023d7eaff0ed26a10ac902720ac9c3037ec7
evan <eevanlaw@gmail.com>
2024-05-07 19:37:40 -0700

    updated README.md and added JavaScript so that the website calculates the values and only accepts numbers

--- exercises/unit-converter/index.js
+++ exercises/unit-converter/index.js
@@ -1,11 +1,22 @@
 const enteredAmount = document.querySelector("#entered-amount");
 const userInput = document.querySelector("#user-input");
 const updateBtn = document.querySelector("#update-btn");
+
 const meterToFeet = document.querySelector("#meter-to-feet");
+const feetToMeter = document.querySelector("#feet-to-meter");
+const literToGallon = document.querySelector("#liter-to-gallon");
+const gallonToLiter = document.querySelector("#gallon-to-liter");
+const kgToLb = document.querySelector("#kg-to-lb");
+const lbToKg = document.querySelector("#lb-to-kg");
 
 let userInputValue = 0;
 
 const convertMeterToFeet = (userInputValue) => userInputValue * 3.28084;
+const convertFeetToMeter = (userInputValue) => userInputValue / 3.28084;
+const convertLiterToGallon = (userInputValue) => userInputValue * 0.264172;
+const convertGallonToLiter = (userInputValue) => userInputValue / 0.264172;
+const convertKgToLb = (userInputValue) => userInputValue * 2.20462;
+const convertLbToKg = (userInputValue) => userInputValue / 2.20462;
 
 userInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
@@ -15,16 +26,23 @@ userInput.addEventListener("keypress", function(event) {
 });
 updateBtn.addEventListener("click", function () {
     userInputValue = Number(userInput.value);
-    enteredAmount.textContent = userInputValue;
-    userInput.value = "";
-    updateText();
+    if (userInputValue === NaN) {
+        enteredAmount.textContent = userInputValue;
+        userInput.value = "";
+        updateText();
+    }
 });
 
 function updateText() {
-    meterToFeet.textContent = generateConversionSentence("meters", "feet", userInputValue, convertMeterToFeet(userInputValue).toFixed(2));
+    meterToFeet.textContent = generateConversionSentence("meters", "feet", userInputValue, convertMeterToFeet(userInputValue));
+    feetToMeter.textContent = generateConversionSentence("feet", "meters", userInputValue, convertFeetToMeter(userInputValue));
+    literToGallon.textContent = generateConversionSentence("liters", "gallons", userInputValue, convertLiterToGallon(userInputValue));
+    gallonToLiter.textContent = generateConversionSentence("gallons", "liters", userInputValue, convertGallonToLiter(userInputValue));
+    kgToLb.textContent = generateConversionSentence("kilograms", "pounds", userInputValue, convertKgToLb(userInputValue));
+    lbToKg.textContent = generateConversionSentence("pounds", "kilograms", userInputValue, convertLbToKg(userInputValue));
 }
 
 function generateConversionSentence(srcUnit, destUnit, srcValue, destValue) {
-    return `${srcValue} ${srcUnit} = ${destValue} ${destUnit}`;
+    return `${srcValue} ${srcUnit} = ${destValue.toFixed(2)} ${destUnit}`;
 }
 
