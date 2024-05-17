c81da83d8915a25df3e3acd2ea7591caf3619622
evan <eevanlaw@gmail.com>
2024-05-16 20:56:25 -0700

    added some CSS to improve the layout

--- exercises/digital-business-card/src/App.jsx
+++ exercises/digital-business-card/src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer.jsx'
+import About from './components/About/About.jsx'
 import Interests from './components/Interests/Interests.jsx'
 
 function App() {
     return (
         <div className='container'>
             <Header />
-
+            <About />
             <Interests />
             <Footer />
         </div>
--- exercises/digital-business-card/src/components/Header/Header.jsx
+++ exercises/digital-business-card/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
+import './header.css'
 
 export default function Header() {
     return (
-        <>
-            <img src='./src/assets/evan.jpg' alt='Evan portrait' />
+        <header>
+            <img id='img-evan' src='./src/assets/evan.jpg' alt='Evan portrait' />
             <h1>Evan Law</h1>
             <h3>Student</h3>
-            <div>
+            <div id='contact-btns'>
                 <div>Email</div>
                 <div>LinkedIn</div>
             </div>
-        </>
+        </header>
     )
 }
