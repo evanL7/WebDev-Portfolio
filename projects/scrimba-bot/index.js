65aee16e1a9e918d96f5188b5a42dcc974c4afea
Evan <eevanlaw@gmail.com>
2024-05-24 15:26:08 -0700

    modified index.js

--- projects/scrimba-bot/index.js
+++ projects/scrimba-bot/index.js
@@ -8,37 +8,48 @@ import dotenv from 'dotenv';
 // Load environment variables from .env file
 dotenv.config();
 
-// Read the contents of the text file
-const filePath = './scrimba-info.txt';
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    progressConversation()
+}) 
 
-(async () => {
-    try {
-        const text = await fs.readFile(filePath, 'utf-8');
-        const splitter = new RecursiveCharacterTextSplitter({
-            chunkSize: 500,
-            chunkOverlap: 50,
-            separators: ['\n\n', '\n', ' ', '']
-        });
+const openAIApiKey = process.env.OPENAI_API_KEY
+const llm = new ChatOpenAI({ openAIApiKey })
 
-        const output = await splitter.createDocuments([text]);
+const standaloneQuestionTemplate = 'Given a question, convert it to a standalone question. question: {question} standalone question:'
+const standaloneQuestionPrompt = PromptTemplate.fromTemplate(standaloneQuestionTemplate)
 
-        const sbApiKey = process.env.SUPABASE_API_KEY;
-        const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT;
-        const openAIApiKey = process.env.OPENAI_API_KEY;
+const answerTemplate = `You are a helpful and enthusiastic support bot who can answer a given question about Scrimba based on the context provided. Try to find the answer in the context. If you really don't know the answer, say "I'm sorry, I don't know the answer to that." And direct the questioner to email help@scrimba.com. Don't try to make up an answer. Always speak as if you were chatting to a friend.
+context: {context}
+question: {question}
+answer: `
+const answerPrompt = PromptTemplate.fromTemplate(answerTemplate)
 
-        const client = createClient(sbUrl, sbApiKey)
+const chain = standaloneQuestionPrompt.pipe(llm).pipe(new StringOutputParser()).pipe(retriever).pipe(combineDocuments).pipe(answerPrompt)
 
-        await SupabaseVectorStore.fromDocuments(
-            output,
-            new OpenAIEmbeddings({ openAIApiKey }),
-            {
-                client,
-                tableName: 'documents',
-            }
-        )
-        console.log("success")
+const response = await chain.invoke({
+    question: 'What are the technical requirements for running Scrimba? I only have a very old laptop which is not that powerful.'
+})
 
-    } catch (err) {
-        console.log(err);
-    }
-})();
+console.log(response)
+
+async function progressConversation() {
+    const userInput = document.getElementById('user-input')
+    const chatbotConversation = document.getElementById('chatbot-conversation-container')
+    const question = userInput.value
+    userInput.value = ''
+
+    // add human message
+    const newHumanSpeechBubble = document.createElement('div')
+    newHumanSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newHumanSpeechBubble)
+    newHumanSpeechBubble.textContent = question
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+
+    // add AI message
+    const newAiSpeechBubble = document.createElement('div')
+    newAiSpeechBubble.classList.add('speech', 'speech-ai')
+    chatbotConversation.appendChild(newAiSpeechBubble)
+    newAiSpeechBubble.textContent = result
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+}
