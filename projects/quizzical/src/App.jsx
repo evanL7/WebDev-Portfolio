60ccec37e0873504cb000964e8c97634c7a90ef6
Evan Law <eevanlaw@gmail.com>
2024-07-07 20:49:23 -0700

    created project: quizzical

---
+++ projects/quizzical/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// Refs:
+// https://opentdb.com/api_config.php
+// https://github.com/mdevils/html-entities
+function App() {
+    return (
+        <>
+        </>
+    )
+}
+
+export default App
